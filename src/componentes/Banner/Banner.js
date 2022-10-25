c84eddd3bcc3992c71e2b228059855c809311545
Débora Veras <80870252+debs-veras@users.noreply.github.com>
2022-10-25 10:31:57 -0300

    projeto organizado e duas componentes criada

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+function Banner(){
+  return (
+    <header className = "Banner">
+      <img src = "/imagens/banner.png" alt = "Esse é o banner principal da página Organo"/>
+    </header>
+  )
+}
+
+export default Banner                           
