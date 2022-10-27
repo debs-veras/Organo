98b15d83b26fe8e1bd5d214e2c88febee03ac54c
Débora Veras <80870252+debs-veras@users.noreply.github.com>
2022-10-27 08:55:12 -0300

    evento finalizado

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -2,6 +2,7 @@ import './Formulario.css'
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import Botao from '../Botao';
+import { useState } from 'react'
 
 const Formulario = () => {
   const times = [
@@ -14,21 +15,25 @@ const Formulario = () => {
     'Inovação e Gestão'
   ]
 
+  const  [nome, setNome] = useState('')
+  const  [cargo, setCargo] = useState('')
+  const  [imagem, setImagem] = useState('')
+  const  [time, setTime] = useState('')
+
   const aoSalvar = (evento) => {
     evento.preventDefault()
+    console.log(nome, cargo, imagem, time)
   } 
 
   return(
     <section className = "formulario">
-      <form onSubmit={aoSalvar}>
-        <h2>
-          Preencha os dados para criar o card dos colaboradores 
-        </h2>
-        <CampoTexto obrigatorio = {true} label = "Nome" placeholder = "Digite seu nome" />
-        <CampoTexto obrigatorio = {true} label = "Cargo" placeholder = "Digite seu cargo" />
-        <CampoTexto label = "Imagem" placeholder = "Digite o endereço da imagem" />
-        <ListaSuspensa obrigatorio = {true} label = "Time" itens = {times} />
-        <Botao texto = "Criar Card"/>
+      <form onSubmit = {aoSalvar}>
+        <h2>Preencha os dados para criar o card dos colaboradores</h2>
+        <CampoTexto obrigatorio = {true} valor = {nome} aoAlterado = {valor => setNome(valor)} label = "Nome" placeholder = "Digite seu nome" />
+        <CampoTexto obrigatorio = {true} valor = {cargo} aoAlterado = {valor => setCargo(valor)} label = "Cargo" placeholder = "Digite seu cargo" />
+        <CampoTexto valor = {imagem} aoAlterado = {valor => setImagem(valor)} label = "Imagem" placeholder = "Digite o endereço da imagem" />
+        <ListaSuspensa obrigatorio = {true} valor = {time} aoAlterado = {valor => setTime(valor)} label = "Time" itens = {times} />
+        <Botao texto = "Criar Card" />
       </form>
     </section>
   )
