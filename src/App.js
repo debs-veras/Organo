d7fe5f6e11c6c8c2e59208c2b9ab9b9ad7701181
Débora Veras <80870252+debs-veras@users.noreply.github.com>
2022-10-27 14:46:31 -0300

    lista dos times finalizado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
-function App() {
+function App(){
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9' 
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82cffa',
+      corSecundaria: '#e8f8ff' 
+    },
+    {
+      nome: 'Data-Science',
+      corPrimaria: '#a6d157',
+      corSecundaria: '#f0f8e2' 
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#e06b69',
+      corSecundaria: '#fde7e8' 
+    },
+    {
+      nome: 'Ux e Desing',
+      corPrimaria: '#d86ebf',
+      corSecundaria: '#fae9f5' 
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#feba05',
+      corSecundaria: '#fff5d9' 
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#ff8a29',
+      corSecundaria: '#ffeedf' 
+    }
+  ]
+
   const [colaboradores, setColaboradores] = useState([]) 
 
   const aoNovoColaboradorCadastrado = (colaborador) => {
-    console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
-    console.log(colaboradores)
   }
 
   return (
     <div className = "App">
       <Banner />
-      <Formulario aoColaboradorCadastrado = {colaborador => aoNovoColaboradorCadastrado(colaborador)}/>
+      <Formulario aoColaboradorCadastrado = {colaborador => aoNovoColaboradorCadastrado(colaborador)} />
+      
+      {times.map(time => <Time key = {time.nome} nome = {time.nome} corPrimaria = {time.corPrimaria} corSecundaria = {time.corSecundaria}/>)}
     </div> 
-  );
+  )
 }
 
 export default App;
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,8 +1,8 @@
-import "./ListaSuspensa.css"
+import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
   return(
-    <div className = 'lista-suspensa'>
+    <div className = "lista-suspensa">
       <label>{props.label}</label>
       <select onChange = {evento => props.aoAlterado(evento.target.value)} required = {props.obrigatorio} value = {props.value}>                 
         {props.itens.map(item => {
