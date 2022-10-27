7b6f2b25171331d88fc5185c7c83da7a61c3aafd
Débora Veras <80870252+debs-veras@users.noreply.github.com>
2022-10-26 22:49:46 -0300

    começando a trabalhar com eventos

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -13,16 +13,21 @@ const Formulario = () => {
     'Mobile',
     'Inovação e Gestão'
   ]
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault()
+  } 
+
   return(
     <section className = "formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>
           Preencha os dados para criar o card dos colaboradores 
         </h2>
-        <CampoTexto label = "Nome" placeholder = "Digite seu nome" />
-        <CampoTexto label = "Cargo" placeholder = "Digite seu cargo" />
+        <CampoTexto obrigatorio = {true} label = "Nome" placeholder = "Digite seu nome" />
+        <CampoTexto obrigatorio = {true} label = "Cargo" placeholder = "Digite seu cargo" />
         <CampoTexto label = "Imagem" placeholder = "Digite o endereço da imagem" />
-        <ListaSuspensa label = "Time" itens = {times} />
+        <ListaSuspensa obrigatorio = {true} label = "Time" itens = {times} />
         <Botao texto = "Criar Card"/>
       </form>
     </section>
