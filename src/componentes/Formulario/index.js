12372909d8796d95a40d1bcbecdeee954d2358f2
Débora Veras <80870252+debs-veras@users.noreply.github.com>
2022-10-27 09:35:56 -0300

    colaboradores criados

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,7 +4,7 @@ import ListaSuspensa from '../ListaSuspensa';
 import Botao from '../Botao';
 import { useState } from 'react'
 
-const Formulario = () => {
+const Formulario = (props) => {
   const times = [
     'Programação',
     'Front-End',
@@ -22,7 +22,9 @@ const Formulario = () => {
 
   const aoSalvar = (evento) => {
     evento.preventDefault()
-    console.log(nome, cargo, imagem, time)
+    props.aoColaboradorCadastrado({
+      nome, cargo, imagem, time
+    })
   } 
 
   return(
